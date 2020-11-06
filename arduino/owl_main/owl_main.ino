49e4fc59c3547fee21cdb9871f3b473f62968f0f
Anton Lenev <m0rg0t.Anton@gmail.com>
2020-11-07 00:11:57 +0300

    beep and blink depending from distance

--- arduino/owl_main/owl_main.ino
+++ arduino/owl_main/owl_main.ino
@@ -2,35 +2,48 @@
 //https://github.com/ErickSimoes/Ultrasonic
 //https://github.com/Martinsos/arduino-lib-hc-sr04
 
-const int LED = 3;
-const int BUZZER = 4;
+
+const int led = 3;
+const int buzzer = 4;
+const int minTime = 10;
+int signalTimeout = 1000;
+int signalCounter = 0;
 
 Ultrasonic ultrasonic(12, 13);
 int distance;
 
 // the setup routine runs once when you press reset:
-void setup()
-{
+void setup() {
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
   pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
 
   Serial.begin(115200);
+  //HCSR04.begin(triggerPin, echoPins, echoCount);
 }
 
 // the loop routine runs over and over again forever:
-void loop()
-{
+void loop() {
   distance = ultrasonic.read();
 
-  digitalWrite(led, HIGH); // turn the LED on (HIGH is the voltage level)
-  //tone(buzzer, 1000);
+  digitalWrite(led, LOW);
+  noTone(buzzer);
 
   Serial.print("Distance: ");
   Serial.println(distance);
-  Serial.println("---");
-  delay(500);
 
-  digitalWrite(led, LOW); // turn the LED off by making the voltage LOW
-  //noTone(buzzer);
+  signalTimeout = minTime * distance;
+  signalCounter = signalCounter + minTime;
+
+  Serial.println("---");
+  delay(minTime);
+
+  if (signalCounter >= signalTimeout) {
+    digitalWrite(led, HIGH);
+    tone(buzzer, 1000);
+    signalCounter = 0;
+    delay(minTime);
+  }
+  
+  //
 }
