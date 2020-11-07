a871ee4a9d98a85f24813fbf35d321f1ab93b4c6
Anton Lenev <m0rg0t.Anton@gmail.com>
2020-11-08 01:46:15 +0300

    Vibro mottors added

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 OwlEye helps to navigate visually impaired people in closed spaces and helps regular people to keep distance on personal communications during covid-19 times.
 
 Hardware+software+art together bring us beautiful owl necklace with integrated sensors, vibro and so on to not only helps people, but also making it in fancy way :)
+
+# 3d model or hand crafted Owl
+
+I create basic 3d model of owl neclace that could be used for 3d printing (made using Fusion 360 CAD editor).
+
+https://a360.co/3eIkNEf - 3d model
--- arduino/owl_main/owl_main.ino
+++ arduino/owl_main/owl_main.ino
@@ -2,21 +2,27 @@
 //https://github.com/ErickSimoes/Ultrasonic
 //https://github.com/Martinsos/arduino-lib-hc-sr04
 
+const int VIBRO_PIN = 2; //Digital pin where vibro attahed
+const int LED_PIN = 3; //Digital pin where led attached
+const int BUZZER_PIN = 4; //Digital pin where buzzer sound stuff is attached
+const int minTime = 3; //minimal amunt of time we use
+const int HC_TRIG_PIN = 12;
+const int HC_ECHO_PIN = 13;
+const int VIBRO_ADDITIONAL_TIME = 10;
 
-const int led = 3;
-const int buzzer = 4;
-const int minTime = 10;
 int signalTimeout = 1000;
-int signalCounter = 0;
+int signalBuzzCounter = 0;
+int signalVibroCounter = 0;
 
-Ultrasonic ultrasonic(12, 13);
+Ultrasonic ultrasonic(HC_TRIG_PIN, HC_ECHO_PIN);
 int distance;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(VIBRO_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT); // Set buzzer - pin 9 as an output
 
   Serial.begin(115200);
   //HCSR04.begin(triggerPin, echoPins, echoCount);
@@ -26,24 +32,29 @@ void setup() {
 void loop() {
   distance = ultrasonic.read();
 
-  digitalWrite(led, LOW);
-  noTone(buzzer);
+  digitalWrite(VIBRO_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+  noTone(BUZZER_PIN);
 
   Serial.print("Distance: ");
   Serial.println(distance);
 
   signalTimeout = minTime * distance;
-  signalCounter = signalCounter + minTime;
-
+  signalBuzzCounter = signalBuzzCounter + minTime;
+  signalVibroCounter =  signalVibroCounter + minTime;
   Serial.println("---");
-  delay(minTime);
 
-  if (signalCounter >= signalTimeout) {
-    digitalWrite(led, HIGH);
-    tone(buzzer, 1000);
-    signalCounter = 0;
-    delay(minTime);
+  //check for vibro motor
+  //for good vibration we need to make it longer then buzzer sounds
+  if (signalVibroCounter >= (signalTimeout - VIBRO_ADDITIONAL_TIME)) {
+    digitalWrite(VIBRO_PIN, HIGH);
   }
-  
-  //
+
+  if (signalBuzzCounter >= signalTimeout) {
+    digitalWrite(LED_PIN, HIGH);
+    tone(BUZZER_PIN, 1000);
+    signalBuzzCounter = 0;
+    signalVibroCounter = 0; //we reset vibro here too
+  }
+  delay(minTime);
 }
